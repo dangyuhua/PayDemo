ffe942f2ed28f62382d1289aff6c04257d3e01e7
dangyuhua <940927264@qq.com>
2018-11-22 13:59:51 +0800

    iOS微信支付宝集成

---
+++ PayDemo/PayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayDemo
+//
+//  Created by 党玉华 on 2018/11/21.
+//  Copyright © 2018年 dyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
